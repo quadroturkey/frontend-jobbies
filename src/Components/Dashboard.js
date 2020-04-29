090e313ef1bfb94a3d786cf639abf8af3359078c
qudroturkey <reesemb1@gmail.com>
2020-04-29 13:23:04 -0500

    user works, trackers unAuth

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
 
 export default class Dashboard extends Component {
-  render(props) {
+  constructor(props) {
+    super(props)
+    this.state = {
+      trackers: []
+    }
+  }
+
+  fetchTrackers() {
+    fetch(`http://localhost:3000/tracker`)
+      .then(r => r.json())
+      .then(response => {
+        localStorage.setItem("token", response.jwt)
+        console.log(response)
+      })
+  }
+
+  componentDidMount() {
+    this.fetchTrackers()
+  }
+
+  render() {
     return (
       <div>
         <h1>Status: {this.props.loggedInStatus}</h1>
         <h1>Username: {this.props.user.username}</h1>
       </div>
+
     )
   }
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -39,13 +39,8 @@ class login extends Component {
     })
       .then(r => r.json())
       .then(response => {
-        // The token below will be used as a header for Authorization in your fetches
-        // If you look in application controller we are requesting the header Authorization
-        // Once it is recieved the token is decrypted and access to data is granted
         localStorage.setItem("token", response.jwt)
-        console.log(response)
         this.setState({ currentUser: response.user.username, loggedIn: true })
-        // console.log(this.props.handleAuth)
         this.props.handleAuth(response)
       })
   }
