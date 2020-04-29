872996350bd0fc78dd3644ad07e2d465f46ab037
qudroturkey <reesemb1@gmail.com>
2020-04-29 17:46:54 -0500

    initial card list view

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Tracker from './Tracker'
+import { Item, Container } from 'semantic-ui-react'
 
 export default class Dashboard extends Component {
   constructor(props) {
@@ -9,11 +11,15 @@ export default class Dashboard extends Component {
   }
 
   fetchTrackers() {
-    fetch(`http://localhost:3000/tracker`)
+    fetch(`http://localhost:3000/profile`, {
+      headers: {
+        'Authorization': `JWT ${localStorage.getItem('token')}`,
+      },
+    })
       .then(r => r.json())
       .then(response => {
-        localStorage.setItem("token", response.jwt)
-        console.log(response)
+        console.log(response.user.trackers)
+        this.setState({ trackers: response.user.trackers })
       })
   }
 
@@ -21,13 +27,26 @@ export default class Dashboard extends Component {
     this.fetchTrackers()
   }
 
+  increment = () =>
+    this.setState((prevState) => ({
+      percent: prevState.percent >= 100 ? 0 : prevState.percent + 20,
+    }))
+
   render() {
     return (
       <div>
+
         <h1>Status: {this.props.loggedInStatus}</h1>
         <h1>Username: {this.props.user.username}</h1>
-      </div>
 
+        <Container>
+          <Item.Group divided>
+            {this.state.trackers.map(tracker =>
+              <Tracker tracker={tracker} key={tracker.id} />
+            )}
+          </Item.Group>
+        </Container>
+      </div>
     )
   }
 }
